30f86b1797b30395a62ae8edcc6c7082ca1a3ae9
@slovacus <slovacus@gmail.com>
2020-08-13 13:19:45 -0500

    refactor: actions for make (#13)

--- provision/make/docs.mk
+++ provision/make/docs.mk
@@ -11,6 +11,7 @@ docs.help:
 	@echo '    Docs:'
 	@echo ''
 	@echo '        docs.show                  Show restview README'
+	@echo '        docs.make                  Make documentation html'	
 	@echo '        docs.terraform             generated docs for terraform'
 	@echo ''
 
@@ -20,4 +21,7 @@ docs.terraform:
 			variables.tf)
 
 docs.show:
-	$(PIPENV_RUN) grip
+	$(PIPENV_RUN) restview ${FILE_README}
+
+docs.make:
+	$(docker-compose) -f ${PATH_DOCKER_COMPOSE}/dev.yml run --rm docs bash -c "cd docs && make html"
